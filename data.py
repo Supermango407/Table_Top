7804acff583f80703c4172f812b658546b5a2943
Supermango407 <150284897+Supermango407@users.noreply.github.com>
2025-02-04 18:34:49 -0600

    added winrate analysis

--- data.py
+++ data.py
@@ -16,27 +16,45 @@ def save_record(game_name:str, players_playing_game:tuple[Player], winner:str, r
         else player.name.lower() 
         for player in players_playing_game
     ]
-
     now = datetime.datetime.now(datetime.timezone.utc)
-    conn = sqlite3.connect('data.db')
-    sql = f"""INSERT INTO 
-        `game_records`(
-            `game`,
-            `players`,
-            `winner`,
-            `date`,
-            `record`
-        ) 
-        VALUES (
-            '{game_name}',
-            '{','.join(player_names)}',
-            '{winner}',
-            '{now}',
-            '{record}');
-    """
-    # print(sql)
 
-    conn.execute(sql)
-    conn.commit()
-    conn.close()
+    with sqlite3.connect('data.db') as conn:
+        sql = f"""INSERT INTO 
+            `game_records`(
+                `game`,
+                `players`,
+                `winner`,
+                `date`,
+                `record`
+            ) 
+            VALUES (
+                '{game_name}',
+                '{','.join(player_names)}',
+                '{winner}',
+                '{now}',
+                '{record}');
+        """
+        # print(sql)
+        conn.commit()
+
+
+def wins_for_pvp(player_1, player_2, game_name):
+    """get how many wins, losses and draws there are for
+    player_1 vs player_2 in game `game`"""
+    # the possible winners for a 1 on 1 game
+    winner_options = ['0', 'tie', '1']
+    games_won = dict()
+
+    with sqlite3.connect('data.db') as conn:
+        cursor = conn.cursor()
+
+        for winner in winner_options:
+            sql = f"""SELECT COUNT() from `game_records`
+            WHERE `players` LIKE '{player_1.name}(%),{player_2.name}(%)'
+            AND `winner` == '{winner}' AND `game` = '{game_name}';"""
+        
+            cursor.execute(sql)
+            games_won[winner] = cursor.fetchall()[0][0]
+        
+        return games_won
 
--- proccess_ai.py
+++ proccess_ai.py
@@ -1,28 +1,29 @@
 from multiprocessing import Pool
 import ai as AI
-from game import Game
+import data
 from othello.othello import Othello, Immanuel
 
 
 # how many games each proccesser handels (1/4 games total)
 games_per_proccesser = 250
-offset = 9000
+seed_start_index = 0
+winner_options = ['0', 'tie', '1']
 
 # AIs
 ais = (
-    AI.Randy,
     Immanuel,
+    AI.Randy,
 )
 
 
-def proccess_game(proccesser):
+def proccess_game(proccesser:int):
     if proccesser % 2 == 0:
         ai_list = ais
     else:
         ai_list = (ais[1], ais[0])
     
     for i in range(games_per_proccesser):
-        seed = proccesser+(i*4)+offset
+        seed = proccesser+(i*4)+seed_start_index
         game = Othello(
             ai_list[0](seed),
             ai_list[1](seed),
@@ -34,8 +35,42 @@ def proccess_game(proccesser):
         game.start_game(True)
 
 
-if __name__ == '__main__':
-    items = [1, 2, 3, 4]
-    with Pool(processes=4) as pool:
-        pool.map(proccess_game, items)
+def analize_winrate(players:tuple):
+    p1_first = data.wins_for_pvp(players[0](), players[1](), "Othello")
+    p2_first = data.wins_for_pvp(players[1](), players[0](), "Othello")
+    
+    player_winnrate = {
+        '0': p1_first['0']+p2_first['1'],
+        'tie': p1_first['tie']+p2_first['tie'],
+        '1': p1_first['1']+p2_first['0'],
+    }
+
+    return player_winnrate
+
+
+def add_winrates(winrate_1:dict, winrate_2:dict):
+    """adds winns losses and ties for `winrate_1` and `winrate_2`."""
+    winrate = dict()
+    for option in winner_options:
+        winrate[option] = winrate_1[option]+winrate_2[option]
+    
+    return winrate
+
+
+def skill_score(winrate:dict):
+    """caculate skill score based off of `winrate`"""
+    if winrate['0'] >= winrate['1']:
+        return winrate['0']/winrate['1'] - 1
+    else:
+        return 1 - winrate['1']/winrate['0']
+
+
+winrate = analize_winrate(ais)
+print(winrate)
+print(skill_score(winrate))
+
+# if __name__ == '__main__':
+    # items = [1, 2, 3, 4]
+    # with Pool(processes=4) as pool:
+    #     pool.map(proccess_game, items)
 
